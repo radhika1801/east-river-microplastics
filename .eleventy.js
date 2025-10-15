beef9966a02f67a7fa2b28692f58941e5b3d0ccd
radhika1801 <malpr026@newschool.edu>
2025-10-15 16:39:49 -0400

    update

--- .eleventy.js
+++ .eleventy.js
@@ -22,7 +22,7 @@ module.exports = function(eleventyConfig) {
 
   // create a filter for our url prefix 
   eleventyConfig.addFilter("prefixedUrl", (url, prefix) => {
-    const finalPrefix = prefix || (isProduction ? "east-river-microplastics/" : "/");
+    const finalPrefix = prefix || (isProduction ? "/east-river-microplastics/" : "/");
     return finalPrefix + url.replace(/^\/+/, "");
   });
 
